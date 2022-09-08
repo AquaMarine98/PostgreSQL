a4a454ca3996407139135f82ae81dc3601305a85
Aqua <vadell57@gmail.com>
2022-09-08 18:17:42 -0300

    errorHandler for sequelize

--- middleware/error.handler.js
+++ middleware/error.handler.js
@@ -1,3 +1,5 @@
+const { ValidationError } = require('sequelize');
+
 function logErrors(err, req, res, next) {
     console.log('logErrors');
     console.error(err);
@@ -8,7 +10,7 @@ function errorHandler(err, req, res, next) {
     console.log('errorHandler');
     res.status(500).json({
         message: err.message,
-        stack: err.errors[0].message,
+        stack: err.stack,
     });
 }
 
@@ -20,8 +22,20 @@ function boomErrorHandler(err, req, res, next) {
     next(err);
 }
 
+function ormErrorHandler(err, req, res, next) {
+    if(err instanceof ValidationError) {
+        res.status(409).json({
+            statusCode: 409,
+            message: err.name,
+            errors: err.errors
+        });
+    }
+    next(err);
+}
+
 module.exports = { 
     logErrors,
     errorHandler,
     boomErrorHandler,
+    ormErrorHandler,
 };
